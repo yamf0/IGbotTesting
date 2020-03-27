4cd1687681058809fb8059da00898ed328ccec38
Yael Martinez <13.ymartinez.2b@gmail.com>
2020-03-27 15:22:49 -0600

    Commit created Class and started instagram In chrome

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ from selenium import webdriver
 path_driver = os.path.dirname(os.path.realpath(__file__))
 print (path_driver)
 
-web_driver = webdriver.Chrome(path_driver + "\chromedriver" )
+class InstaComment ():
+    def __init__(self):
+        self.web_driver = webdriver.Chrome(path_driver + "\chromedriver\chromedriver.exe" )
+        self.web_driver.get("https://instagram.com")
 
+InstaComment()
