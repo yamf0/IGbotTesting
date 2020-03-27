c31376248311db2f380a22a2cd25e4adcf5bdc75
Yael Martinez <13.ymartinez.2b@gmail.com>
2020-03-27 16:25:56 -0600

    Add username and pw

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 
 import os
+from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common import keys
 
 #Find PATH to current Directory (to find the dirver)
 path_driver = os.path.dirname(os.path.realpath(__file__))
 print (path_driver)
 
 class InstaComment ():
-    def __init__(self):
+    def __init__(self, username, pw):
         self.web_driver = webdriver.Chrome(path_driver + "\chromedriver\chromedriver.exe" )
         self.web_driver.get("https://instagram.com")
+        sleep(5)
+        self.web_driver.find_element_by_name("username").send_keys(username)
+        self.web_driver.find_element_by_name("password").send_keys(pw)
+        
 
-InstaComment()
+
+Bot = InstaComment('mexicansombreroless','mannheimzittau')
