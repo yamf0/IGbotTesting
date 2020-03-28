57005e3b06c42f019987283a15524343f1e28983
ZEH1SI <HugoArmando.ZepedaRuiz@de.bosch.com>
2020-03-28 14:24:01 +0100

    Me está dando ahorita problemas el comentario

--- main.py
+++ main.py
@@ -9,15 +9,23 @@ path_driver = os.path.dirname(os.path.realpath(__file__))
 print (path_driver)
 
 class InstaComment ():
-    def __init__(self, username, pw):
+    def __init__(self, username, pw,hashtag,comment):
         self.web_driver = webdriver.Chrome(path_driver + "\chromedriver\chromedriver.exe" )
         self.web_driver.get("https://instagram.com")
-        sleep(5)
+        sleep(2)
         self.web_driver.find_element_by_name("username").send_keys(username)
         self.web_driver.find_element_by_name("password").send_keys(pw)
-        self.web_driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button/div")\
-            .click()
-        
+        self.web_driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button/div").click()
+        sleep(3)
+        self.web_driver.find_element_by_xpath("/html/body/div[4]/div/div/div[3]/button[2]").click()
+        self.web_driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input").send_keys(hashtag)
+        sleep(1)
+        self.web_driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[2]/div[2]/div[2]/div/a[1]/div").click()
+        sleep(3)
+        self.web_driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div/div[2]").click()
+        sleep(3)
+        #self.web_driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/div[2]/section[3]/div/form/textarea").send_keys(comment)
+        self.web_driver.find_element_by_tag_name("textarea").send_keys(comment)
+        #self.web_driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/div[2]/section[3]/div/form/button").click()
 
-
-Bot = InstaComment('mexicansombreroless','mannheimzittau')
+Bot = InstaComment('mexicansombreroless','mannheimzittau','#travelling','lovely photo')
