ac01f381826d5878900b52b916b32286a768bc29
Yael Martinez <13.ymartinez.2b@gmail.com>
2020-03-28 06:56:56 -0600

    Added dict containing hastag and comments

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ import os
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common import keys
+import random
 
 #Find PATH to current Directory (to find the dirver)
 path_driver = os.path.dirname(os.path.realpath(__file__))
 print (path_driver)
+
+poss = ["#stayandwander", "#europe_perfection","#landscape", "#travel", "#travelphotography"]
+comm = {
+    "#stayandwander" : ["what an amazing pic!", "Perfection", "We loved it"],
+    "#europe_perfection": ["This is amazing", "Congrats for the great photo", "What a Pic!!!"],
+    "#landscape": ["Wow that pic!", "This place is amazing", "Keep up the great photos"],
+    "#travel": ["Perfect!", "Amazing", "That is nice!"],
+    "#travelphotography": ["What a destination", "That is amazing", "Great!!!"]
+}
 
 class InstaComment ():
     def __init__(self, username, pw):
@@ -17,7 +27,28 @@ class InstaComment ():
         self.web_driver.find_element_by_name("password").send_keys(pw)
         self.web_driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button/div")\
             .click()
-        
+        sleep(2)
+        self.web_driver.find_element_by_xpath("/html/body/div[4]/div/div/div[3]/button[2]").click()
+        used_hs = self.hashtag()  
+        print(used_hs)
+        used_comment = self.comment(used_hs)
+        print(used_comment)
+
+    def hashtag(self):
+        """
+            Will choose randomly a Hashtag and rerturn its value
+        """
+        choose = random.randint(0,len(poss))
+        return poss[choose]
+    
+    def comment (self,hs):
+        """
+            Will choose a comment depending on the hashtag that was opened
+        """
+        comment = comm[hs]
+        num = random.randint(0,len(comment))
+        comment = comment[num]
+        return comment
 
 
 Bot = InstaComment('mexicansombreroless','mannheimzittau')
