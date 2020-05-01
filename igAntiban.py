3efe960f1fc5e4af678ea51b035a426773009a29
hugoA01228679 <hugozepeda6@gmail.com>
2020-05-01 03:33:56 +0200

    Antiban file

---
+++ igAntiban.py
@@ -0,0 +1,41 @@
+'''
+    Developers: Yael Abelardo Martínez & Hugo Armando Zepeda Ruiz
+    Created: 03,2020
+    Purpose: Automation of interaction in Instagram from Mexican Sombrero & -less, Testing
+    Copyright
+'''
+from igStart import igStart
+
+import os
+#Library to control the timings of execution
+from time import sleep
+#Principal library for web scrapping
+from selenium import webdriver
+from selenium.webdriver.common import keys
+from selenium.webdriver.remote.command import Command
+from selenium.common.exceptions import NoSuchElementException
+#Libraries used to avoid being banned
+import random
+import argparse
+import math
+#Library to print personalize message. Allows more control in message control 
+import logging
+from logging import StreamHandler
+
+class igAntiban(igStart):
+    """
+        Class that avoids banning
+    """
+    def __init__(self):
+        #Close the Photo
+        self.web_driver.find_element_by_xpath("/html/body/div[4]/div[3]/button").click()
+        sleep(2)
+        #Click Return "Instagram Button"
+        self.exceptionHandler("/html/body/div[1]/section/nav/div[2]/div/div/div[1]/a")
+        #logger.info("returned to Main page")
+        #self.web_driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[1]/a/div/div/img").click()
+        sleep(2)
+        self.exceptionHandler("/html/body/div[1]/section/main/section/div[1]/div/div/div/div[1]/button")
+        #logger.info("start checking histories, expected return at: {} minutes".format(localtime()[4] + 5))
+        sleep(300)
+        self.exceptionHandler("/html/body/div[1]/section/div/div/section/div[2]/button[3]")  
